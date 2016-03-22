93c8617ec26de9ab549fc379afe9054c01f0ac4a
Professor Taki <Taki21@users.noreply.github.com>
2016-03-22 16:13:07 -0400

    Added files via upload

---
+++ src/main/java/cn/nukkit/network/protocol/AdventureSettingsPacket.java
@@ -0,0 +1,28 @@
+package cn.nukkit.network.protocol;
+
+/**
+ * @author Nukkit Project Team
+ */
+public class AdventureSettingsPacket extends DataPacket {
+
+    public static final byte NETWORK_ID = ProtocolInfo.ADVENTURE_SETTINGS_PACKET;
+
+    public int flags;
+
+    @Override
+    public void decode() {
+
+    }
+
+    @Override
+    public void encode() {
+        reset();
+        putInt(flags);
+    }
+
+    @Override
+    public byte pid() {
+        return NETWORK_ID;
+    }
+
+}
